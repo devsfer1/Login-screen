6d4007ff715fb220bc7a3e41e41e7298074007a9
FernandoChaves <fernando.chaves16@hotmail.com>
2021-05-17 20:04:15 -0300

    fix interpolatores and gesture handler

--- src/services/routes/stack.routes.tsx
+++ src/services/routes/stack.routes.tsx
@@ -1,13 +1,45 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+  createStackNavigator,
+  TransitionSpecs,
+  TransitionPresets,
+  CardStyleInterpolators,
+} from '@react-navigation/stack';
 
 import Initial from '../../screens/out/Initial';
 import Login from '../../screens/out/Login';
+import {Easing} from 'react-native-reanimated';
 
 const stackRoutes = createStackNavigator();
 
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+const closeConfig = {
+  animation: 'timing',
+  config: {
+    duration: 500,
+    easing: Easing.linear,
+  },
+};
+
 const AppRoutes: React.FC = () => (
-  <stackRoutes.Navigator headerMode="none">
+  <stackRoutes.Navigator
+    screenOptions={{
+      gestureEnabled: 'true',
+      gestureDirection: 'horizontal',
+      cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+    }}
+    headerMode="none">
     <stackRoutes.Screen name="Initial" component={Initial} />
     <stackRoutes.Screen name="Login" component={Login} />
   </stackRoutes.Navigator>
