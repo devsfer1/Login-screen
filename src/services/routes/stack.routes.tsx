8d5b24050126fd6087c033c13441054f2dedfc85
FernandoChaves <fernando.chaves16@hotmail.com>
2021-05-17 20:05:49 -0300

    remove unused

--- src/services/routes/stack.routes.tsx
+++ src/services/routes/stack.routes.tsx
@@ -1,36 +1,13 @@
 import React from 'react';
 import {
   createStackNavigator,
-  TransitionSpecs,
-  TransitionPresets,
   CardStyleInterpolators,
 } from '@react-navigation/stack';
 
 import Initial from '../../screens/out/Initial';
 import Login from '../../screens/out/Login';
-import {Easing} from 'react-native-reanimated';
 
 const stackRoutes = createStackNavigator();
-
-const config = {
-  animation: 'spring',
-  config: {
-    stiffness: 1000,
-    damping: 50,
-    mass: 3,
-    overshootClamping: false,
-    restDisplacementThreshold: 0.01,
-    restSpeedThreshold: 0.01,
-  },
-};
-
-const closeConfig = {
-  animation: 'timing',
-  config: {
-    duration: 500,
-    easing: Easing.linear,
-  },
-};
 
 const AppRoutes: React.FC = () => (
   <stackRoutes.Navigator
